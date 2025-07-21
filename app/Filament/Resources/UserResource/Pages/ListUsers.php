ee8ac9c60b3d3cb8f867797c53485a18ed4eccb7
Yusuf Bayu Wicaksono <yusuf.wicaksono@tarunabakti.or.id>
2025-07-21 10:04:00 +0700

    add role

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -8,6 +8,7 @@ use App\Imports\MyUserCreateImport;
 use App\Imports\MyUserImport;
 use Filament\Resources\Pages\ListRecords;
 use EightyNine\ExcelImport\ExcelImportAction;
+use App\Models\User;
 
 class ListUsers extends ListRecords
 {
@@ -28,6 +29,18 @@ class ListUsers extends ListRecords
                 ->icon('heroicon-o-arrow-up-tray')
                 ->use(MyUserCreateImport::class)
                 ->hidden(!$userAuth->hasRole(['super_admin', 'guru_bk'])),
+            // add button to update user role to panel_user
+            Actions\Action::make('updateRole')
+                ->label('Update Role')
+                ->icon('heroicon-o-user-plus')
+                ->color('primary')
+                ->action(function () {
+                    $users = User::where('role', '!=', 'panel_user')->get();
+                    foreach ($users as $user) {
+                        $user->assignRole('panel_user');
+                    }
+                })
+                ->hidden(!$userAuth->hasRole(['super_admin'])),
             Actions\CreateAction::make(),
         ];
     }
